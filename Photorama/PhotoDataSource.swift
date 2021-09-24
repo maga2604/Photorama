7db69c4721d83cc54811c24fd403e80a895e50d1
Maga <maga.rajic2604@gmail.com>
2021-09-24 11:02:39 -0700

    API and Collection Views

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Maga Rajic on 24.09.2021..
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "PhotoCollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PhotoCollectionViewCell
+        cell.update(displaying: nil)
+        return cell
+    }
+}
